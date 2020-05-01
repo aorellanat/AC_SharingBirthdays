b988df03ee9d1f0f5d50ddba1316701b8328f712
Adriana <adrianaorellana34@gmail.com>
2020-05-01 14:29:11 -0400

    search birthday added

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import FormBirthday from '../views/FormBirthday.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'FormBirthday',
-    component: FormBirthday
-  }
-]
+  const routes = []
 
 const router = new VueRouter({
   routes
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,18 +9,37 @@
         color="teal lighten-2"
       ></v-date-picker>
       <br>
-      <v-btn class="button" color="teal lighten-2" style="color: white">Find your birthday twin!</v-btn>
+      <v-btn class="button" color="teal lighten-2" @click="searchBirthday()" style="color: white">Find your birthday twin!</v-btn>
     </v-container>
   </div>
 </template>
 
 <script>
+
+import data from '@/assets/data/open.json'
+
 export default {
   name: 'Home',
   data: () => ({
     picker: new Date().toISOString().substr(0, 10)
   }),
-  components: {
+  methods: {
+    searchBirthday: function () {
+      var date = this.picker.split('-').map(Number);
+      date[1] = date[1] - 1;
+
+      var characters = data.series14.filter(function (character) {
+        var birthday = new Date(character.birthday);
+
+        if (birthday.getMonth() == date[1] && birthday.getDate() == date[2]) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+
+      console.log(characters);
+    }
   }
 }
 </script>
